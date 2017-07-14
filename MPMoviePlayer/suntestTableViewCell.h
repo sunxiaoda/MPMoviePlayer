85b6e91307b941285557a8ed1be8c894a557704c
sunny <2334242@qq.com>
2017-07-14 11:40:43 +0800

    add

---
+++ MPMoviePlayer/suntestTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  suntestTableViewCell.h
+//  MPMoviePlayer
+//
+//  Created by 聚艺互动 on 2017/7/14.
+//  Copyright © 2017年 聚艺互动. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface suntestTableViewCell : UITableViewCell
+
+@end
---
+++ MPMoviePlayer/suntestTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  suntestTableViewCell.m
+//  MPMoviePlayer
+//
+//  Created by 聚艺互动 on 2017/7/14.
+//  Copyright © 2017年 聚艺互动. All rights reserved.
+//
+
+#import "suntestTableViewCell.h"
+
+@implementation suntestTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
