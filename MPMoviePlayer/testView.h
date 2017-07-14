547ee02d77a8053f6839fd4f58490d145e090ee9
sunny <2334242@qq.com>
2017-07-14 17:09:31 +0800

    ggit

---
+++ MPMoviePlayer/testView.h
@@ -0,0 +1,13 @@
+//
+//  testView.h
+//  MPMoviePlayer
+//
+//  Created by 聚艺互动 on 2017/7/14.
+//  Copyright © 2017年 聚艺互动. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testView : UIView
+
+@end
---
+++ MPMoviePlayer/testView.m
@@ -0,0 +1,22 @@
+
+//
+//  testView.m
+//  MPMoviePlayer
+//
+//  Created by 聚艺互动 on 2017/7/14.
+//  Copyright © 2017年 聚艺互动. All rights reserved.
+//
+
+#import "testView.h"
+
+@implementation testView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
